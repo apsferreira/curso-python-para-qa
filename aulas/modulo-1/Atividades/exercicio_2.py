76f6663ff020b00eb1ba4a397d1bed9c103be0fb
Bojakko <lcmt917@gmail.com>
2024-12-12 16:36:42 -0400

    Ajuste de Pastas e Exercícios Atualizados
    	new file:   aulas/modulo-1/Atividades/exercicio_1.py
    	new file:   aulas/modulo-1/Atividades/exercicio_2.py
    	renamed:    exercicio_2.py -> aulas/modulo-1/Atividades/exercicio_3.py
    	deleted:    aulas/modulo-1/codigos/aula-1/exercicio-1.py
    	deleted:    aulas/modulo-1/codigos/aula-1/exercicio-2.py

---
+++ aulas/modulo-1/Atividades/exercicio_2.py
@@ -0,0 +1,6 @@
+nome = input("Digite o seu nome: ")
+ano_nascimento = int(input("Digite o ano do seu nascimento: "))
+
+idade = 2024 - ano_nascimento
+
+print(f"Olá, {nome}! Você tem {idade} anos!")
