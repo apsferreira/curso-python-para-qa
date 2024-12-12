2cc9b3eed45abd1f502c61795834ded3d3af4f04
LoJeronymo <lucas.jeronymo@eldorado.org.br>
2024-12-12 17:34:55 -0300

    meu primeiro commit

--- contribuicoes/ex_2.py
+++ contribuicoes/ex_2.py
@@ -1,6 +1,3 @@
-# escreva um programa que a entrada será a idade do usuario e o retorno do programa será uma mensagem informando se o
-# usuario pode possuir uma permissao para dirigir ou nao
-
 idade = int(input("Digite sua idade: "))
 
 if idade >= 18:
