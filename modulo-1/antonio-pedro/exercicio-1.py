c57090f06ec0416d4dc56c521266406c6b705d74
Antonio Pedro Ferreira <apsf88@gmail.com>
2024-12-12 12:34:01 -0300

    exercicios do modulo 1 aula 1

---
+++ modulo-1/antonio-pedro/exercicio-1.py
@@ -0,0 +1,20 @@
+nome = input("informe o seu nome: ")
+
+print(f"ola, {nome}! Bem vindo ao curso!")
+print("sua idade atual eh: " + str(idade))
+
+
+
+sudo apt update
+sudo apt install python3
+sudo apt install python3-pip
+
+curl https://pyenv.run | bash
+
+he work 
+fundador do basecamp
+
+
+nome = "João"
+ Você tem {idade} anos.")
+# Saída: Olá, João! Você tem
