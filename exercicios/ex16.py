8f73439d2f37613b4f100fcbae3ed25375157db1
LoJeronymo <lucas.jeronymo@eldorado.org.br>
2024-12-17 08:49:10 -0300

    exercicios feitos

---
+++ exercicios/ex16.py
@@ -0,0 +1,5 @@
+#16.Tendo como dados de entrada a altura de uma pessoa, construa um algoritmo quecalcule seu peso ideal, usando a seguinte fórmula: (72.7*altura) - 58
+
+altura = float(input("Digite sua altura em metros: "))
+peso_ideal = (72.7 * altura) - 58
+print("Seu peso ideal é:", peso_ideal)
---
+++ exercicios/ex7.py
@@ -0,0 +1,7 @@
+#7.Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
+
+lado = float(input("Digite o lado do quadrado: "))
+area_quadrado = lado ** 2
+dobro_area = area_quadrado * 2
+print("A área do quadrado é:", area_quadrado)
+print("O dobro da área é:", dobro_area)
