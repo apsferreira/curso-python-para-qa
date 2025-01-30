0eafc8afa09643b77cc487a3b51322e0030b5ea5
psilDave <davipsilva7@gmail.com>
2025-01-30 15:12:42 -0300

    Merge remote-tracking branch 'origin/davi-pereira' into davi-pereira

--- contribuicoes/aula02/exercicios-aula/ex03.py
+++ contribuicoes/aula02/exercicios-aula/ex03.py
@@ -1,10 +1,13 @@
 print("CALCULADORA - OPERAÇÕES BÁSICAS\n\n")
 
+operacao = input("Digite a operação desejada: + | - | * | / | // | % | ^ |: ")
 num1 = float(input("Digite o primeiro número: "))
 num2 = float(input("Digite o segundo número: "))
 
+
 print("*********************************************\n")
 print(f"Os números foram: {num1} e {num2}\n\n")
+
 print(f'SOMA {num1} + {num2} = {round(num1+num2, 2)}')
 print(f'SUBTRAÇÃO {num1} - {num2} = {round(num1-num2, 2)}')
 print(f'MULTIPLICAÇÃO {num1} * {num2} = {round(num1*num2, 2)}')
@@ -17,3 +20,22 @@ else:
     print(f'RESTO DA DIVISAO {num1} % {num2} = {round(num1%num2, 2)}')
 
 print(f'POTENCIACÃO {num1} ^ {num2} = {round(num1**num2, 2)}')
+
+if operacao == "+":
+    print(f'SOMA {num1} + {num2} = {round(num1+num2, 2)}')
+elif operacao == "-":
+    print(f'SUBTRAÇÃO {num1} - {num2} = {round(num1-num2, 2)}')
+elif operacao == "*":
+    print(f'MULTIPLICAÇÃO {num1} * {num2} = {round(num1*num2, 2)}')
+elif operacao == "^":
+    print(f'POTENCIACÃO {num1} ^ {num2} = {round(num1**num2, 2)}')
+if num2 == 0:
+    print(f"Não é possivel realizar: DIVISÃO, DIVISÃO INTEIRA e RESTO DA DIVISAO, pois segundo número é {num2}")
+else:
+    if operacao == "/":
+        print(f'DIVISÃO {num1} / {num2} = {round(num1/num2, 2)}')
+    elif operacao == "//":
+        print(f'DIVISÃO INTEIRA {num1} // {num2} = {round(num1//num2, 2)}')
+    elif operacao == "%":
+        print(f'RESTO DA DIVISAO {num1} % {num2} = {round(num1%num2, 2)}')
+>>>>>>> d065666 (Melhorando a calculadora, definindo a operação.)
