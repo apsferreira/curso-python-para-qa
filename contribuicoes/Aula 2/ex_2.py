8fe81d85524c0fc4127be87339a4fb9fd5e4212e
LoJeronymo <lucas.jeronymo@eldorado.org.br>
2025-01-30 14:32:18 -0300

    mudança estrutura de pastas

--- contribuicoes/Aula 2/ex_2.py
+++
@@ -1,6 +0,0 @@
-idade = int(input("Digite sua idade: "))
-
-if idade >= 18:
-    print('Você possui idade suficiente para dirigir')
-else:
-    print('Você ainda não possui idade suficiente para dirigir')
