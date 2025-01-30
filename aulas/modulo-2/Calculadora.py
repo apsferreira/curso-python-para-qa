35da07441b836dc1a32e8f6580f8ec6f92e8f149
Bojakko <lcmt917@gmail.com>
2025-01-30 14:53:21 -0400

    .

---
+++ aulas/modulo-2/Calculadora.py
@@ -0,0 +1,7 @@
+n1 = float(input("Digite um valor:"))
+operacao = str(input("Digite o operador:"))
+n2 = float(input("Digite um valor:"))
+
+if  operacao == "+":    
+    print("O resultado da soma é:", n1+n2)
+elif 
