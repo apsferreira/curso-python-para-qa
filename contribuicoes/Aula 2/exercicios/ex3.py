8986b2f6d88760a3533f79ee59dfc5efae85a592
LoJeronymo <lucas.jeronymo@eldorado.org.br>
2025-02-04 14:20:11 -0300

    mudança de pasta

--- contribuicoes/Aula 2/exercicios/ex3.py
+++
@@ -1,6 +0,0 @@
-#3.Faça um Programa que peça dois números e imprima a soma.
-
-num1 = float(input("Digite o primeiro número: "))
-num2 = float(input("Digite o segundo número: "))
-soma = num1 + num2
-print("A soma é:", soma)
--- contribuicoes/Aula 2/exercicios/ex5.py
+++
@@ -1,5 +0,0 @@
-#5.Faça um Programa que converta metros para centímetros.
-
-metros = float(input("Digite o valor em metros: "))
-centimetros = metros * 100
-print(metros, "metros equivalem a", centimetros, "centímetros")
