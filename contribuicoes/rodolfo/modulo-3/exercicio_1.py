afd50876323423b6bb8e666aec065c14ed06b793
rodolfo <rodolfozacarias@gmail.com>
2025-02-04 15:47:45 -0300

    [CursoPython|Modulo3] - funções   - cria uma função de saudação padronizada

---
+++ contribuicoes/rodolfo/modulo-3/exercicio_1.py
@@ -0,0 +1,6 @@
+# Crie uma funçao que imprima uma saudação, recebendo como parametro o nome do usuário.
+
+def saudacao(nome_usuario):
+    print(f"Olá {nome_usuario}, seja bem vindo ao sistema de testes!")
+
+saudacao(input("Qual o seu nome: "))
